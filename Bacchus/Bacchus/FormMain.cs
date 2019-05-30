b1347e4b22dbaf145e249d712b84150d6eb63e68
= <remito@live.fr>
2019-05-30 18:02:04 +0200

    debut trie

--- Bacchus/Bacchus/FormMain.cs
+++ Bacchus/Bacchus/FormMain.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
+using System.Windows.Forms;
 
 namespace Bacchus
 {
     public partial class FormMain : Form
     {
+
+        private ListViewColumnSorter lvwColumnSorter;
+
+
         public FormMain()
         {
             InitializeComponent();
+            lvwColumnSorter = new ListViewColumnSorter();
+            this.itemsView.ListViewItemSorter = lvwColumnSorter;
 
         }
 
@@ -42,31 +50,28 @@ namespace Bacchus
         #region Items treeView
         private void itemsView_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            Console.WriteLine("sender:" + sender.ToString());
-            Console.WriteLine("e:" + e.ToString());
             string selectedNodeText = e.Node.Text;
-            Console.WriteLine("selected:" + selectedNodeText);
 
             switch (selectedNodeText)
             {
                 case "Articles":
-                    Console.WriteLine("Show products");
+                    Console.WriteLine("Display products");
                     displayProducts();
                     break;
                 case "Familles":
-                    Console.WriteLine("Show families");
+                    Console.WriteLine("Display families");
                     displayFamilies();
                     break;
                 case "Sous familles":
-                    Console.WriteLine("Show families");
+                    Console.WriteLine("Display subfamilies");
                     displaySubfamilies();
                     break;
                 case "Marques":
-                    Console.WriteLine("Show families");
+                    Console.WriteLine("Display brands");
                     displayBrands();
                     break;
                 default:
-                    Console.WriteLine("Nothing");
+                    Console.WriteLine("Display nothing");
                     break;
             }
         }
@@ -245,6 +250,34 @@ namespace Bacchus
         }
 
 
+        private void itemsView_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            // Determine if clicked column is already the column that is being sorted.
+
+            Console.WriteLine("e.Column:"+ e.Column);
+            Console.WriteLine("lvwColumnSorter.SortColumn:" + lvwColumnSorter.SortColumn);
+            if (e.Column == lvwColumnSorter.SortColumn)
+            {
+                // Reverse the current sort direction for this column.
+                if (lvwColumnSorter.Order == SortOrder.Ascending)
+                {
+                    lvwColumnSorter.Order = SortOrder.Descending;
+                }
+                else
+                {
+                    lvwColumnSorter.Order = SortOrder.Ascending;
+                }
+            }
+            else
+            {
+                // Set the column number that is to be sorted; default to ascending.
+                lvwColumnSorter.SortColumn = e.Column;
+                lvwColumnSorter.Order = SortOrder.Ascending;
+            }
+
+            // Perform the sort with these new sort options.
+            this.itemsView.Sort();
+        }
 
 
         private void Form1_Load(object sender, System.EventArgs e)
@@ -263,4 +296,116 @@ namespace Bacchus
         }
         #endregion
     }
+}
+
+
+
+
+/// <summary>
+/// This class is an implementation of the 'IComparer' interface.
+/// </summary>
+public class ListViewColumnSorter : IComparer
+{
+    /// <summary>
+    /// Specifies the column to be sorted
+    /// </summary>
+    private int ColumnToSort;
+    /// <summary>
+    /// Specifies the order in which to sort (i.e. 'Ascending').
+    /// </summary>
+    private SortOrder OrderOfSort;
+    /// <summary>
+    /// Case insensitive comparer object
+    /// </summary>
+    private CaseInsensitiveComparer ObjectCompare;
+
+    /// <summary>
+    /// Class constructor.  Initializes various elements
+    /// </summary>
+    public ListViewColumnSorter()
+    {
+        // Initialize the column to '0'
+        ColumnToSort = 0;
+
+        // Initialize the sort order to 'none'
+        OrderOfSort = SortOrder.None;
+
+        // Initialize the CaseInsensitiveComparer object
+        ObjectCompare = new CaseInsensitiveComparer();
+    }
+
+    /// <summary>
+    /// This method is inherited from the IComparer interface.  It compares the two objects passed using a case insensitive comparison.
+    /// </summary>
+    /// <param name="x">First object to be compared</param>
+    /// <param name="y">Second object to be compared</param>
+    /// <returns>The result of the comparison. "0" if equal, negative if 'x' is less than 'y' and positive if 'x' is greater than 'y'</returns>
+    public int Compare(object x, object y)
+    {
+        int compareResult;
+        ListViewItem listviewX, listviewY;
+
+        // Cast the objects to be compared to ListViewItem objects
+        listviewX = (ListViewItem)x;
+        listviewY = (ListViewItem)y;
+
+        decimal num = 0;
+        if (decimal.TryParse(listviewX.SubItems[ColumnToSort].Text, out num))
+        {
+            compareResult = decimal.Compare(num, Convert.ToDecimal(listviewY.SubItems[ColumnToSort].Text));
+        }
+        else
+        {
+            // Compare the two items
+            compareResult = ObjectCompare.Compare(listviewX.SubItems[ColumnToSort].Text, listviewY.SubItems[ColumnToSort].Text);
+        }
+
+        // Calculate correct return value based on object comparison
+        if (OrderOfSort == SortOrder.Ascending)
+        {
+            // Ascending sort is selected, return normal result of compare operation
+            return compareResult;
+        }
+        else if (OrderOfSort == SortOrder.Descending)
+        {
+            // Descending sort is selected, return negative result of compare operation
+            return (-compareResult);
+        }
+        else
+        {
+            // Return '0' to indicate they are equal
+            return 0;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the number of the column to which to apply the sorting operation (Defaults to '0').
+    /// </summary>
+    public int SortColumn
+    {
+        set
+        {
+            ColumnToSort = value;
+        }
+        get
+        {
+            return ColumnToSort;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the order of sorting to apply (for example, 'Ascending' or 'Descending').
+    /// </summary>
+    public SortOrder Order
+    {
+        set
+        {
+            OrderOfSort = value;
+        }
+        get
+        {
+            return OrderOfSort;
+        }
+    }
+
 }
